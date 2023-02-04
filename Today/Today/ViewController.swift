59a2217f89c3f985d2b2b78a2d0c8c82e4a94a7c
junha <goodjunha@gmail.com>
2023-02-04 16:12:29 +0900

    📦:: 프로젝트 세팅

---
+++ Today/Today/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by 박준하 on 2023/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
