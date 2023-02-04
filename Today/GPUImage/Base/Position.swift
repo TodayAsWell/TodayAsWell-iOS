26e4566065342d37d8ed3400aa075c8d4e515dbe
junha <goodjunha@gmail.com>
2023-02-04 19:01:18 +0900

    🦕:: GPUimage 추가

---
+++ Today/GPUImage/Base/Position.swift
@@ -0,0 +1,34 @@
+//
+//  Position.swift
+//  GPUImage
+//
+//  Created by 박준하 on 2023/02/04.
+//
+import Foundation
+
+#if os(iOS)
+import UIKit
+#endif
+
+public struct Position {
+    public let x:Float
+    public let y:Float
+    public let z:Float?
+    
+    public init (_ x:Float, _ y:Float, _ z:Float? = nil) {
+        self.x = x
+        self.y = y
+        self.z = z
+    }
+    
+#if !os(Linux)
+    public init(point:CGPoint) {
+        self.x = Float(point.x)
+        self.y = Float(point.y)
+        self.z = nil
+    }
+#endif
+    
+    public static let center = Position(0.5, 0.5)
+    public static let zero = Position(0.0, 0.0)
+}
---
+++ Today/GPUImage/Base/Size.swift
@@ -0,0 +1,16 @@
+//
+//  Size.swift
+//  GPUImage
+//
+//  Created by 박준하 on 2023/02/04.
+//
+
+public struct Size {
+    public let width:Float
+    public let height:Float
+    
+    public init(width:Float, height:Float) {
+        self.width = width
+        self.height = height
+    }
+}
